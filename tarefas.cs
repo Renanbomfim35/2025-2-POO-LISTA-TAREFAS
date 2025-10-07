4a08f8d0aab5c33c674c6f09769ebce730a9ae68
Renanbomfim35 <renan.bomfim@yahoo.com.br>
2025-10-07 00:21:11 +0000

    feat:tarefas

---
+++ tarefas.cs
@@ -0,0 +1,20 @@
+public class Tarefa
+{
+    public Id Int {get; set; }
+
+    public Nome String {get; set;}
+    
+    public Descricao String {get; set;}
+
+    public dataCriacao DateTime  {get; set;}
+
+    public status Int {get; set;}
+    
+    public dataExecucao DateTime {get; set;}
+    
+    
+    
+    
+    
+
+}
---
+++ tarefas/operacoes.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+
+public class Operacoes
+{
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+
+    public Tarefa Buscar(int Id)
+    {
+        return null;
+
+    }
+    public List<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>()
+
+
+        }
+
+    public void Alterar(ActivityTraceFlags tarefa)
+    {
+
+    }
+
+    public void Excluir(int Id)
+    {
+            
+        }
+
+}
